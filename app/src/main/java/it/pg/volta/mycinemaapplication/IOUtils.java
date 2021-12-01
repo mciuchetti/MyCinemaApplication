4a99c771edca75ba1487387a0b2d7b09bdf5eb2b
Monica Ciuchetti <monica.ciuchetti@gmail.com>
2021-12-01 07:25:04 +0100

    Caricamento iniziale

---
+++ app/src/main/java/it/pg/volta/mycinemaapplication/IOUtils.java
@@ -0,0 +1,24 @@
+package it.pg.volta.mycinemaapplication;
+
+import java.io.InputStream;
+import java.io.Reader;
+
+public class IOUtils {
+
+    public static void closeQuietly(InputStream in)  {
+        try {
+            in.close();
+        }catch (Exception e) {
+
+        }
+    }
+
+    public static void closeQuietly(Reader reader)  {
+        try {
+            reader.close();
+        }catch (Exception e) {
+
+        }
+    }
+
+}
